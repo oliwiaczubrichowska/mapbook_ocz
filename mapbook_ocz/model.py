ab898419f2e9bb3d31c730eb7533a5c45542abf5
oliwiaczubrichowska <oliwia.czubrichowska@student,wat,edu,pl>
2026-05-07 10:36:45 +0200

    chore: refactoring project structure to nvc

---
+++ mapbook_ocz/model.py
@@ -0,0 +1,8 @@
+users=[
+    {"name":"oliwia","location":"skierniewice",
+     "posts":["sprzedma mercedasa", "kupie skrzynie biegow", "ratunku co robic po wypadku"]},
+    {"name":"daniel","location":"legionowo",
+     "posts":["moj kod nie dziala pomocya"]},
+    {"name":"kamil","location":"debica",
+     "posts":["czy ktos zrobil sprawozdanie"]},
+]
